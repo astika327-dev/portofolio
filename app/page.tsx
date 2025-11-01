1d3cf4b97313ccb1c15dfc0283c1f96ae4c90ef2
astika327-dev <astika327@gmail.com>
2025-11-01 17:34:50 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,17 @@ import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import Background from "./components/Background";
 
 export default function Home() {
   return (
-    <main>
-      <Background />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </div>
     </main>
   );
 }
